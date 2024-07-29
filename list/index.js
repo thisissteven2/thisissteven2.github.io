135e516f572b5b8233bd0b8ee7700bf236a6c87d
steven2801 <ssteven075@gmail.com>
2024-07-29 13:54:39 +0700

    add script

--- list/index.js
+++ list/index.js
@@ -1,30 +1,78 @@
 const axios = require("axios");
 const { load } = require("cheerio");
 const fs = require("fs");
+const path = require("path");
 
 // const uri = "https://langeek.co/en/vocab/topic-related";
-const uri = "https://langeek.co/en/vocab/book-collection/16/adverbs";
+const uri = "https://langeek.co/en/vocab/category/187/adverbs-time-place";
+
+// TODO: Get all categories per id by parsing html
+/**
+ * structure:
+ *  {
+      "id": 525,
+      "title": "Large Mammals",
+      "wordsCount": 25,
+      "estimatedLearningTime": 750,
+    },
+ */
+
+const folderPath = path.join(__dirname, "category");
 
 async function main() {
-  //   const { data } = await axios.get(uri);
+  const { data } = await axios.get(uri);
 
-  //   const $ = load(data);
+  const $ = load(data);
 
-  //   const scriptText = $("#__NEXT_DATA__").text();
+  // Select the <main> element
+  const mainElement = $("main");
 
-  //   console.log(scriptText);
-  //   const parsed = JSON.parse(scriptText);
-  //   fs.writeFileSync("data.json", JSON.stringify(parsed, null, 2));
-  const categories = JSON.parse(fs.readFileSync("categories.json"));
+  // Get the first two children
+  const firstChild = mainElement.children().eq(0);
+  const secondChild = mainElement.children().eq(1);
 
-  fs.writeFileSync(
-    "bookCollections.json",
-    JSON.stringify(
-      categories.sort((a, b) => a.id - b.id),
-      null,
-      2
-    )
-  );
+  // Extract the text from the first child
+  const title = firstChild.find("h1").text().trim();
+  const description = firstChild.find("div.tw-text-sm").text().trim();
+
+  const containerDivs = secondChild.find("div.tw-w-full.tw-relative.tw-basis-full");
+
+  // Create an array to store the extracted data
+  const list = [];
+
+  containerDivs.each((index, element) => {
+    const div = $(element);
+
+    // Extract the adverb title
+    const adverbTitle = div.find("h3").text().trim();
+
+    // Extract words and time
+    const wordsElement = div.find("p.tw-text-sm.tw-text-gray-58").first();
+    const timeElement = div.find("p.tw-text-sm.tw-text-gray-58").last();
+
+    const wordsText = wordsElement.text().trim();
+    const timeText = timeElement.text().trim();
+
+    // Extract numbers from text if available
+    const wordsMatch = wordsText.match(/(\d+)/);
+    const timeMatch = timeText.match(/(\d+)/);
+
+    const words = wordsMatch ? wordsMatch[1] : "N/A";
+    const time = timeMatch ? timeMatch[1] : "N/A";
+
+    // Extract the ID from the href attribute
+    const href = div.find("a[href]").attr("href");
+    const id = href ? href.split("/")[4] : "N/A";
+
+    list.push({
+      id,
+      adverbTitle,
+      words,
+      time,
+    });
+  });
+
+  fs.writeFileSync(path.join(folderPath, "187.json"), JSON.stringify({ title, description, list }, null, 2));
 }
 
 main().catch(console.error);
